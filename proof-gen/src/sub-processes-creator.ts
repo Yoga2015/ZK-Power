5a2cd18eea6b36e51b12f5060faf64cb311b6053
Yoga <94358089+Yoga2015@users.noreply.github.com>
2023-10-20 15:31:58 +0800

    Determine have ${cores} cores available

--- proof-gen/src/sub-processes-creator.ts
+++ proof-gen/src/sub-processes-creator.ts
@@ -29,7 +29,7 @@ export type SubProcessCordinator = {
 type WorkerStatus = 'IsReady' | 'Busy';
 
 export const createSubProcesses = async (n: number) => {
-    let cores = os.cpus().length - 2;
+    let cores = os.cpus().length - 4;
     logger.info(`Number of CPUs is ${cores}`);
     logger.info(`Master ${process.pid} is running`);
     if (cores <= n) {
