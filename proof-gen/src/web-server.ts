4b40c215b81fbd5313bdb039bebfc3299463d9d3
Yoga2015 <yoga3333@126.com>
2023-10-21 11:59:27 +0800

    improve it to configure graphyQL endpoint

--- proof-gen/src/web-server.ts
+++ proof-gen/src/web-server.ts
@@ -2,10 +2,14 @@ import { FastifyCore } from './app'
 import { activeMinaInstance } from "@tokenizk/util";
 
 const server = async () => {
-    // init Mina tool
 
-        
+    // init Mina tool 
+    try {
         await activeMinaInstance(); // TODO improve it to configure graphyQL endpoint
+    } catch (error) {
+        console.log(error);
+        
+    }
 
    
 
