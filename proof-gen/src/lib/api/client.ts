ebc00d857522af977e09a61914c8bf0bf2171b6a
Yoga2015 <yoga3333@126.com>
2023-10-28 12:30:37 +0800

    Import third-party modules

--- proof-gen/src/lib/api/client.ts
+++ proof-gen/src/lib/api/client.ts
@@ -1,5 +1,5 @@
 import config from '../config';
-import axios from 'axios';
+// import axios from 'axios';
 
 export const $axiosCoreService = axios.create({
     baseURL: `${config.httpProtocol}://${config.coreServiceHost}:${config.coreServicePort}`,
