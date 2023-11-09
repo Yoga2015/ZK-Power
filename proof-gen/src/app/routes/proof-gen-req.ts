bb05dca893379c5186c545c53c9c75547470343d
Tang1903 <246968103@qq.com>
2023-11-09 23:08:27 +0800

    Print error message when sending fails

--- proof-gen/src/app/routes/proof-gen-req.ts
+++ proof-gen/src/app/routes/proof-gen-req.ts
@@ -29,8 +29,8 @@ const handler: RequestHandler<ProofTaskDto<any, any>, null> = async function (
 
     try {
         (process as any).send(req.body);
-    } catch (error) {
-        console.log(error);
+    } catch (err) {
+        console.log(err);
 
     }
 
